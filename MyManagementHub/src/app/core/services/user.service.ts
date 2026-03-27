fa135762772aece8d55e5a9a6b7727b2fe281563
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-28 00:33:31 +0100

    Create user service for create new user when sign up

---
+++ MyManagementHub/src/app/core/services/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { DtoInputSignUp } from '../../pages/sign-up/dto/dto-input-signup';
+import { Observable, tap } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private static _BASE_URL_API = environment.BASE_URL_API + "user";
+
+  private _isConnected = signal(false);
+  isConnected = this._isConnected.asReadonly();
+
+  constructor(private _httpClient: HttpClient) {}
+
+  signup(dto: DtoInputSignUp) {
+    return this._httpClient
+      .post(`${UserService._BASE_URL_API}`, dto, { withCredentials: true }) 
+      .pipe(
+        tap(() => this._isConnected.set(false))
+      );
+  }
+}
