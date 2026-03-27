cdb8aad9844223a92d91837e7b9e323d9394f884
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-28 00:31:01 +0100

    Create routes

--- MyManagementHub/src/app/app.routes.ts
+++ MyManagementHub/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { roleGuard } from './core/guards/role.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		loadComponent: () =>
+			import('./layout/main-layout/main-layout.component').then((m) => m.MainLayout),
+		canActivate: [roleGuard],
+	},
+	{
+		path: 'sign-in',
+		loadComponent: () =>
+			import('./pages/sign-in/sign-in.component').then((m) => m.SignIn),
+	},
+	{
+		path: 'sign-up',
+		loadComponent: () =>
+			import('./pages/sign-up/sign-up.component').then((m) => m.SignUp),
+	},
+	{
+		path: '**',
+		redirectTo: '',
+	},
+];
