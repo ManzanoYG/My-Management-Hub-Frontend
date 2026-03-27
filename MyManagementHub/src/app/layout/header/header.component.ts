30f0947fc9abd05723c677fba7ed955c81e22757
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-28 00:34:19 +0100

    Create responsive sidebar menu and top fix bar when mobile

---
+++ MyManagementHub/src/app/layout/header/header.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, effect, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { JwtRoleService } from '../../core/auth/jwt-role.service';
+
+@Component({
+  selector: 'app-header',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgOptimizedImage, RouterLink, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class Header {
+  private _authService = inject(AuthService);
+  private _jwtRoleService = inject(JwtRoleService);
+  private _cdr = inject(ChangeDetectorRef);
+
+  isConnected = this._authService.isConnected;
+  username = signal<string | null>(null);
+
+  constructor() {
+    effect(() => {
+      if (this.isConnected()) {
+        this._jwtRoleService.getUsername().subscribe(username => {
+          this.username.set(username);
+          this._cdr.markForCheck();
+        });
+      }
+    });
+  }
+
+  logout() {
+    this._authService.logout().subscribe();
+    window.location.reload();
+  }
+}
