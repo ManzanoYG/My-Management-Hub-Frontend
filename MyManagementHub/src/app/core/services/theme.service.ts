330a5e513582f9a0319fcf47327e0c2aee55dcd2
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-30 15:53:33 +0200

    Create retro theme

--- MyManagementHub/src/app/core/services/theme.service.ts
+++ MyManagementHub/src/app/core/services/theme.service.ts
@@ -1,7 +1,7 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
-export type Theme = 'light' | 'dark';
+export type Theme = 'light' | 'dark' | 'retro';
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +14,13 @@ export class ThemeService  {
   private themes: Record<Theme, string> = {
     light: 'light-theme',
     dark: '',
+    retro: 'retro-theme'
   };
 
   private logoPaths: Record<Theme, string> = {
     light: 'assets/images/myManagementHubLogo_alt.webp',
-    dark: 'assets/images/myManagementHubLogo.webp'
+    dark: 'assets/images/myManagementHubLogo.webp',
+    retro: 'assets/images/myManagementHubLogo_alt.webp'
   };
 
   constructor(@Inject(PLATFORM_ID) platformId: Object) {
