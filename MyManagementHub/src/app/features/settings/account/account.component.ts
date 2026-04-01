8a9b8dfe4829efe177c7e6be361e9bc21be43eea
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-01 17:15:16 +0200

    Add method to delete user account

--- MyManagementHub/src/app/features/settings/account/account.component.ts
+++ MyManagementHub/src/app/features/settings/account/account.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Button } from "../../../shared/ui/button/button.component";
+import { UserService } from '../../../core/services/user.service';
+import { JwtRoleService } from '../../../core/auth/jwt-role.service';
+import { AuthService } from '../../../core/services/auth.service';
+import { Modal } from "../../../shared/ui/modal/modal.component";
 
 @Component({
   selector: 'app-account',
-  imports: [Button],
+  imports: [Button, Modal],
   templateUrl: './account.component.html',
   styleUrl: '../settings.component.css',
 })
 export class Account {
-  test(): void {
-    console.log('Button pressed');
+  private _jwtRoleService = inject(JwtRoleService);
+  private _userService = inject(UserService);
+  private _authService = inject(AuthService);
+  private username = signal<string | null>(null);
+  bodyDeleteAccount = "Are you sure you want to delete your account? This action is irreversible and all your data will be permanently lost.";
+  errorDeleteAccount = '';
+
+  constructor() {
+    this._jwtRoleService.getUsername().subscribe(username => {
+        this.username.set(username);
+    });
+  }
+
+  deleteAccount(): void {
+    this._userService.deleteUser({ username: this.username()! }).subscribe({
+      next: (response) => {
+        if(response.deleted) {
+          this._authService.logout().subscribe();
+          window.location.reload();
+        } else {
+          this.errorDeleteAccount = "Failed to delete user.";
+        }
+      },
+      error: (error) => {
+        this.errorDeleteAccount = "An error occurred while deleting the account.";
+      }
+    });
   }
 }
