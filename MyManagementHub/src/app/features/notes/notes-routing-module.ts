4fb6be7e1d6efdd495d19767cabfed2930bd556c
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-08 01:23:02 +0200

    Add routing to create note component

--- MyManagementHub/src/app/features/notes/notes-routing-module.ts
+++ MyManagementHub/src/app/features/notes/notes-routing-module.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { roleGuard } from '../../core/guards/role.guard';
 import { isAuthenticatedGuard } from '../../core/guards/authenticated.guard';
-import { isNotAuthenticatedGuard } from '../../core/guards/not-authenticated.guard';
 
 export const NotesRouting: Routes = [
   {
-		path: '',
-		loadComponent: () => import('./pages/notes-home/notes-home.component').then((m) => m.NotesHome),
-    canActivate: [isAuthenticatedGuard, roleGuard]
-	}
+    path: '',
+    loadComponent: () => import('./pages/notes-home/notes-home.component').then((m) => m.NotesHome),
+    canActivate: [isAuthenticatedGuard, roleGuard],
+  },
+  {
+    path: 'create',
+    loadComponent: () => import('./pages/create-note/create-note.component').then((m) => m.CreateNote),
+    canActivate: [isAuthenticatedGuard, roleGuard],
+  },
 ];
 
